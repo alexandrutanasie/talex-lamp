014069336b3119007cfb378bee4cee1e282dd602
Alexandru Tanasie <alexandrutanasiee@gmail.com>
2023-11-27 11:16:37 +0200

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -5,14 +5,32 @@ apt update
 apt install apache2
 service apache2 start
 
-
 #Installing MySQL 8.0
 apt update
 apt install mysql-server
 service mysql start
 
-
 #Installing PHP with Apache
 apt update
 apt install php libapache2-mod-php
 apt install openssl php-common php-bcmath php-curl php-json php-mbstring php-mysql php-tokenizer php-xml php-zip php-mbstring php-dom php-cli php-gd php-xml php-json php-mcrypt
+
+#add virtual host
+echo """
+<VirtualHost *:80>
+    ServerAdmin admin@example.com
+    ServerName site.example.com
+    DocumentRoot /var/www/site.example.com/public
+
+    <Directory /var/www/site.example.com>
+        Options Indexes MultiViews FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+""" > /etc/apache2/sites-available/site.example.com.conf
+#restart apache2
+service apache2 reload
